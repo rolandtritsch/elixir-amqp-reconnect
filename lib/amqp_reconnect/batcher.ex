c16f9df22517b6fe5f51eaf1a496da469bb8b296
Roland Tritsch <roland@tritsch.email>
2022-01-07 13:17:37 +0000

    Adding ability to ack (and resend) batches.

--- lib/amqp_reconnect/batcher.ex
+++ lib/amqp_reconnect/batcher.ex
@@ -2,6 +2,11 @@ defmodule AmqpReconnect.Batcher do
   @moduledoc """
   This batcher is a gen_stage producer. It waits for demand to
   arrive and will then create batches of payloads to process.
+
+  Note: To get the at-least-once semantic we want, we need to
+  add the ability to resend batches that were not completely
+  processed by the publisher. We do this by keeping a copy
+  of the current batch around (in the state).
   """
 
   use GenStage
@@ -18,17 +23,29 @@ defmodule AmqpReconnect.Batcher do
 
   @impl true
   def init(_args) do
-    {:producer, 0}
+    {:producer, []}
   end
 
+  # Create the batch to process.
   @impl true
-  def handle_demand(demand, count) do
-    # Create the batch to process.
+  def handle_demand(demand, []) do
     timestamp = NaiveDateTime.utc_now
     batch = 1..demand |> Enum.map(fn i ->
       "payload - #{i} - #{timestamp}"
     end)
 
-    {:noreply, batch, count + demand}
+    {:noreply, batch, batch}
+  end
+
+  # Batch was not acked. Resend.
+  @impl true
+  def handle_demand(_demand, batch) do
+    {:noreply, batch, batch}
+  end
+
+  # Ack the batch. Next handle_demand will send a new batch
+  @impl true
+  def handle_call(:ack, _from, _batch) do
+    {:reply, :ok, [], []}
   end
 end
--- lib/amqp_reconnect/publisher.ex
+++ lib/amqp_reconnect/publisher.ex
@@ -1,5 +1,15 @@
 defmodule AmqpReconnect.Publisher do
-  @moduledoc false
+  @moduledoc """
+  This stage publishes batches of events.
+
+  Can be started and stopped at will (this is why it is transient).
+
+  Note: If the publisher dies (e.g. because the channel/infrastructure
+  dies) it will be restarted by the supervisor. So far so good. But it
+  has lost the batch it failed to process. To make this work we keep
+  the batch around in the batcher until it is acked by the publisher.
+  See below.
+  """
 
   use GenStage, restart: :transient
 
@@ -23,23 +33,26 @@ defmodule AmqpReconnect.Publisher do
   # --- callbacks 
 
   @impl true
-  def init(state) do
-    {:consumer, {connect(), state}, subscribe_to: [{AmqpReconnect.Batcher, max_demand: 10}]}
+  def init(:state_doesnt_matter) do
+    {:consumer, connect(), subscribe_to: [{AmqpReconnect.Batcher, max_demand: 10}]}
   end
 
   @impl true
-  def handle_events(events, _from, {channel, state}) do
+  def handle_events(events, _from, channel) do
     events |> Enum.each(fn event ->
       Logger.info("Publish: #{event}")
       :ok = AMQP.Basic.publish(channel, "amq.fanout", "#", event)
       Process.sleep(1_000)
     end)
 
-    {:noreply, [], {channel, state}}
+    # Let the batcher know that we are done with this batch
+    :ok = GenStage.call(AmqpReconnect.Batcher, :ack)
+
+    {:noreply, [], channel}
   end
 
   @impl true
-  def terminate(:normal, {channel, _state}) do
+  def terminate(:normal, channel) do
     connection = channel.conn
     AMQP.Channel.close(channel)
     AMQP.Connection.close(connection)
@@ -47,12 +60,20 @@ defmodule AmqpReconnect.Publisher do
     :terminate_does_not_return_anything
   end 
 
+  @impl true
+  def terminate(_reason, _channel) do
+    :terminate_does_not_return_anything
+  end
+
   # --- private functions
      
   defp connect() do
     {:ok, connection} = AMQP.Connection.open()
     {:ok, %AMQP.Channel{pid: cpid} = channel} = AMQP.Channel.open(connection)
+
+    # Register the channel with a name, so that we can kill it
     Process.register(cpid, @channel_name)
+
     channel
   end
 end
